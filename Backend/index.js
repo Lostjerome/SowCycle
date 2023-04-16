69a5509d3371505d8351c508c7eb0884e4ec4310
Jerome <88102079+Lostjerome@users.noreply.github.com>
2023-04-16 11:42:48 +0700

    :heavy_plus_sign: Setup database connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "sowcycle",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+
+// set /api as the base route
+app.use("/api", (req, res, next) => {
+  console.log("Request received at /api");
+  next();
+});
+
+// GET /api/pigs
+app.get("/pigs", (req, res) => {
+  console.log("Request received at /api/pigs");
+  connection.query("SELECT * FROM pigs", (err, results) => {
+    if (err) {
+      console.error("Error retrieving pigs:", err);
+      res.status(500).send("Error retrieving pigs");
+    } else {
+      console.log("Pigs retrieved successfully");
+      res.status(200).json(results);
+    }
+  });
 });
 
 app.listen(3000, () => {
