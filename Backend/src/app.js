343181650dc0394c95927e9bb6085c4481b2ef40
Jerome <88102079+Lostjerome@users.noreply.github.com>
2023-06-03 17:56:31 +0700

    :lock: Allow cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,10 @@
 require("module-alias/register");
 
 const express = require("express");
-
 const requireDir = require("require-dir");
 const routes = requireDir("routes");
-
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv").config();
 require("@config/database").connect();
@@ -15,6 +14,12 @@ const { API_PORT } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+    cors({
+        // allowed all origins for development
+        origin: "*",
+    })
+);
 
 Object.keys(routes).forEach((key) => {
     app.use(`/api/${key}`, routes[key]);
